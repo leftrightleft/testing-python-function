ce3579586daa322312293e6f166174c03f2b507d
Dan Shanahan <leftrightleft@github.com>
2023-05-11 16:53:09 -0700

    Update main.py

--- main.py
+++ main.py
@@ -4,25 +4,30 @@ from urllib.parse import urlparse, parse_qs
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
 def connect_to_db(request):
+    # Parse the query parameters
     query_components = parse_qs(urlparse(request.path).query)
     name = query_components["parameter"][0] if "parameter" in query_components else None
-# Open a connection to the database
-    database_uri = os.environ.get('SQLITE_URI', ':memory:')
 
+    # Open a connection to the database
+    database_uri = os.environ.get('SQLITE_URI', ':memory:')
     database = sqlite3.connect(database_uri, check_same_thread=False)
 
     # Execute a SQL query
     cursor = database.cursor()
     cursor.execute(
-            "SELECT * FROM books WHERE name LIKE '%" + name + "%'"
-        )
+        "SELECT * FROM books WHERE name LIKE '%" + name + "%'"
+    )
 
     # Iterate over the rows and print the results
     for x in cursor:
         print(x)
 
-def im_a_teapot():
-    print("I'm a teapot!")
+def run_server():
+    # Start the web server
+    server_address = ("", 8000)
+    httpd = HTTPServer(server_address, connect_to_db)
+    print("Server running on port 8000")
+    httpd.serve_forever()
 
-if __name__ == '__main__':
-    im_a_teapot()
+if __name__ == "__main__":
+    run_server()
